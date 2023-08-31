cdeeef7c73217343cdfa6215db1ee9420e046fc1
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-08-31 15:17:29 -0400

    update

--- install-plugins.sh
+++ install-plugins.sh
@@ -5,5 +5,7 @@ wp plugin install show-current-template --activate --allow-root
 wp plugin install query-monitor --activate --allow-root
 
 chown -R www-data:www-data /var/www/html/wp-content
+chown -R 1000:1000 /var/www/html/wp-content/plugins/$PLUGIN_SLUG
+
 find /var/www/html/wp-content -type d -exec chmod 755 {} \;
 find /var/www/html/wp-content -type f -exec chmod 644 {} \;
