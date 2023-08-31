a3d195ef82dc9ad76f135cb63c46f7cd33d26d33
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-08-31 02:40:13 -0400

    update header

--- plugin/Awesome-Plugin.php
+++ plugin/Awesome-Plugin.php
@@ -1,4 +1,15 @@
 <?php
 /**
  * Plugin Name: Awesome Plugin
+ * Plugin URI: https://github.com/WilliamSampaio/wp-plugin-base
+ * Description: A Awesome Plugin for you!
+ * Version: 0.9.9
+ * Requires at least: 6.3.1
+ * Requires PHP: 8.0
+ * Author: Me Myself and I
+ * Author URI: https://github.com/WilliamSampaio
+ * License: Buy me a coffee sometime :)
+ * License URI: https://github.com/WilliamSampaio
+ * Text Domain: awesome-plugin
+ * Domain Path: /languages
  */
